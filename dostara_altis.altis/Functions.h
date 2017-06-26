3788e0366213e5bdfa5c5cba2621bd290e626f86
Shems <Shemsxvs@icloud.com>
2017-06-26 21:13:42 +0200

    part client delete admin menu

--- dostara_altis.altis/Functions.h
+++ dostara_altis.altis/Functions.h
@@ -59,24 +59,6 @@ class Life_Client_Core
 		class welcomeNotification {};
 	};
 
-	class Admin
-	{
-		 file = "core\admin";
-        class admininfo {};
-        class adminid {};
-        class admingetID {};
-        class adminMenu {};
-        class adminQuery {};
-        class adminSpectate {};
-        class adminTeleport {};
-        class adminTpHere {};
-        class adminDebugCon {};
-        class adminCompensate {};
-        class adminGodMode {};
-        class adminFreeze {};
-        class adminMarkers {};
-	};
-
 	class Medical_System
 	{
 		file = "core\medical";
--- dostara_altis.altis/core/admin/fn_admingetID.sqf
+++
@@ -1,9 +0,0 @@
-#include "..\..\macro.h"
-
-private "_unit";
-_unit = getSelData(2902);
-_unit = call compile format["%1", _unit];
-if(isNil "_unit") exitwith {};
-if(isNull _unit) exitWith {};
-
-[_unit,player] RemoteExecCall ["TON_fnc_getID",0];
